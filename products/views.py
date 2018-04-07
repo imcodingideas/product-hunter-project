9ced40cca395ef4bd8eb5fd0d48621d919dbfadd
Joseph Chambers <joseph@michael-chambers.com>
2018-04-07 14:36:14 -0700

    Create Product

--- products/views.py
+++ products/views.py
@@ -1,5 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Product
+from django.utils import timezone
 
 # Create your views here.
 def home(request):
   return render(request, 'products/home.html')
+
+@login_required
+def create(request):
+  if request.method == 'POST':
+    title = request.POST['title']
+    body = request.POST['body']
+    url = request.POST['url']
+    icon = request.FILES['icon']
+    image = request.FILES['image']
+
+    if title and body and url and icon and image:
+      product = Product()
+      product.title = title
+      product.body = body
+
+      if url.startswith('http://') or url.startswith('https://'):
+        product.url = url
+      else:
+        product.url = 'http://' + url
+
+      product.icon = request.FILES['icon']
+      product.image = request.FILES['image']
+      product.pub_date = timezone.datetime.now()
+      product.hunter = request.user
+      product.save()
+      return redirect('home')
+
+    else:
+      return render(request, 'products/create.html', {'error': 'All fields are required.'})
+
+  else:
+    return render(request, 'products/create.html')
